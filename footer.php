31f14f4b8301b80751f0681af14d4da2f0d9197a
Mohit Pal Singh <mohit.pal.singh@outlook.com>
2023-07-18 03:42:16 +0530

    first_commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="blockquote-footer fixed-bottom">LinkedIn - <cite title="Source Title"><a href="https://linkedin.com/in/mohit-pal-singh" target="_blank">Mohit Pal Singh</a></cite></footer>
+</body>
+</html>
